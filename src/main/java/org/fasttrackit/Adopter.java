31198a2a8d6935ce53f73937efcdf83ff27de43f
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-10 19:18:24 +0300

    metod added

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -7,4 +7,15 @@ public class Adopter {
     int age;
     double moneyBugget;
 
+
+
+
+    //"Ramona just gave some pedigree Chicken and Rice Flavor to Paco"
+    public <food> int feed(String pet, food) {
+        return 0;
+System.out.println (String + " is feeding the " + name + "with" + "pedigree Chiken and Rice Flower" +  "Paco";
+
 }
+}
+
+
--- src/main/java/org/fasttrackit/Pet.java
+++ src/main/java/org/fasttrackit/Pet.java
@@ -12,4 +12,6 @@ public class Pet {
     int age;
     double spiritLevel;
 
+
+
 }
