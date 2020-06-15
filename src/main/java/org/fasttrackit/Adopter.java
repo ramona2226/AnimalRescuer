5bd6f52161b700a2b6f793e4747853871b1961b4
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-15 20:14:45 +0300

    spirit level after recreation activity

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -43,10 +43,34 @@ public class Adopter {
         }
 
 
-     //  public void recreationActivity(Pet petReference, Activity activityReference) {
+       public void recreation(Pet petReference, Activity activityReference) {
+            // System.out.println("Ramona is making a recreation activity with Paco.");
+
+if (activityReference.name.equals(petReference.preferedActivity)) {
+
+    System.out.println("Spirit level before recreation activity is:" +petReference.spiritLevel);
 
 
-              // System.out.println("Ramona is making a recreation activity with Paco.");
+    System.out.println("This is my prefered activity!");
+petReference.spiritLevel +=2;
+
+
+    System.out.println("Spirit level after recreation activity is:" + petReference.spiritLevel);
+
+} else {
+    System.out.println("Spirit level  before unprefered activity is:");
+    System.out.println("This activity is not my prefered activity!");
+
+petReference.spiritLevel -=1;
+
+    System.out.println("Spirit level after unprefered activity is: ");
+
+
+
+}
+
+}
+
 
 
 
