82c2218318038587b348dda7e4ae66a037497b25
Ramona2226 <brisc.ramona22@yahoo.com>
2020-07-11 14:55:17 +0300

    polimotfism added

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -14,136 +14,136 @@ public class App {
                game.start();
 
 
-//        Pet petReference = new Pet("Paco", 7, 9, 10);
-//
-//
-//         petReference.setName("Paco");
-//        petReference.setBreed("Shih tzu");
-//        petReference.setColor("white");
-//        petReference.setAge(4);
-//        petReference.setPreferedFood("Pedigree");
-//          petReference.setHealtyLevel(7);
-//        petReference.setHungryLevel(9);
-//        petReference.setSpiritLevel(10);
-//        petReference.setPreferedActivity("catch the ball");
-//
-//
-//        System.out.println(petReference.getName());
-//        System.out.println(petReference.getBreed());
-//        System.out.println(petReference.getColor());
-//        System.out.println(petReference.getAge());
-//        System.out.println(petReference.getPreferedFood());
-//        System.out.println(petReference.getHealtyLevel());
-//        System.out.println(petReference.getHungryLevel());
-//        System.out.println(petReference.getSpiritLevel());
-//        System.out.println(petReference.getPreferedActivity());
-//
-//
-//        petReference.jumping();
-//
-//
-//        Activity activityReference = new Activity("catch the ball");
-//
-//        activityReference.setName("catch the ball");
-//        activityReference.setType("stimulating the mind");
-//
-//        System.out.println(activityReference.getName());
-//        System.out.println(activityReference.getType());
-//
-//
-//        RecreationActivity recreationActivityReference = new RecreationActivity("catch the ball");
-//
-//        recreationActivityReference.setName("catch the ball");
-//        recreationActivityReference.setType("Stimulation the mind");
-//
-//
-//        EntertainmentActivity entertainmentActivity = new EntertainmentActivity();
-//        entertainmentActivity.setName("Run");
-//        entertainmentActivity.setTime(30);
-//
-//
-//        System.out.println(recreationActivityReference.getName());
-//        System.out.println(recreationActivityReference.getType());
-//
-//        Adopter adopterReference = new Adopter("Ramona");
-//adopterReference.setName("Ramona");
-//        adopterReference.setGender("female");
-//        adopterReference.setMoneyBugget(10000);
-//
-//        System.out.println(adopterReference.getName());
-//        System.out.println(adopterReference.getGender());
-//        System.out.println(adopterReference.getAge());
-//        System.out.println(adopterReference.getMoneyBugget());
-//
-//
-//        LocalDate expirationDate = LocalDate.of(2050, 6, 6);
-//        Food foodReference = new Food("Pedigree", 29, 35, expirationDate);
-//        //foodReference.setName("Pedigree");
-//
-//
-//        foodReference.setDisponibility("True");
-//       foodReference.setPrice(29);
-//        foodReference.setGramsQuantity(35);
-//        foodReference.expirationDate = LocalDate.of(2050, 6, 6);
-//
-//        System.out.println(foodReference.getName());
-//        System.out.println(foodReference.isDisponibility());
-//        System.out.println(foodReference.getPrice());
-//        System.out.println(foodReference.isDisponibility());
-//        System.out.println(foodReference.getExpirationDate());
-//
-//
-//        adopterReference.feed(petReference, foodReference);
-//
-//        adopterReference.recreation(petReference, activityReference);
-//
-//
-//        System.out.println(petReference.getSpiritLevel());
-//
-//
-//            foodReference.expirationDate = LocalDate.now().plusYears(1).plusMonths(3).minusDays(7);
-//
-//
-//        Toys toys = new Toys("red", "squezze");
-//       toys.setColor("red");
-//       toys.setSound("squezze");
-//
-//        System.out.println(toys.getName());
-//
-//        System.out.println(toys.getColor());
-//        System.out.println(toys.getSound());
-//
-//        Snack snack = new Snack("soft", expirationDate);
-//        snack.setTexture("soft");
-//        System.out.println(snack.getTexture());
-//
-//      Dog dog = new Dog("David", 5,5,5);
-//        dog.tag = "necklace";
-//
-//        System.out.println(dog.getName());
-//        System.out.println(dog.getHealtyLevel());
-//        System.out.println(dog.getHungryLevel());
-//        System.out.println(dog.getSpiritLevel());
-//        System.out.println(dog.tag);
-//
-//
-//        System.out.println(dog.toString());
-//
-//
-//
-//
-//        Cat cat = new Cat("Kitty", 8, 3, 7);
-//        cat.hairAccesorise = "bow clips";
-//
-//        System.out.println(cat.getName());
-//        System.out.println(cat.getHealtyLevel());
-//        System.out.println(cat.getHungryLevel());
-//        System.out.println(cat.getSpiritLevel());
-//
-//        System.out.println(cat.hairAccesorise);
-//
-//        cat.jumping();
-//
+        Pet petReference = new Pet("Paco", 7, 9, 10);
+
+
+         petReference.setName("Paco");
+        petReference.setBreed("Shih tzu");
+        petReference.setColor("white");
+        petReference.setAge(4);
+        petReference.setPreferedFood("Pedigree");
+          petReference.setHealtyLevel(7);
+        petReference.setHungryLevel(9);
+        petReference.setSpiritLevel(10);
+        petReference.setPreferedActivity("catch the ball");
+
+
+        System.out.println(petReference.getName());
+        System.out.println(petReference.getBreed());
+        System.out.println(petReference.getColor());
+        System.out.println(petReference.getAge());
+        System.out.println(petReference.getPreferedFood());
+        System.out.println(petReference.getHealtyLevel());
+        System.out.println(petReference.getHungryLevel());
+        System.out.println(petReference.getSpiritLevel());
+        System.out.println(petReference.getPreferedActivity());
+
+
+        petReference.jumping();
+
+
+        Activity activityReference = new Activity("catch the ball");
+
+        activityReference.setName("catch the ball");
+        activityReference.setType("stimulating the mind");
+
+        System.out.println(activityReference.getName());
+        System.out.println(activityReference.getType());
+
+
+        RecreationActivity recreationActivityReference = new RecreationActivity("catch the ball");
+
+        recreationActivityReference.setName("catch the ball");
+        recreationActivityReference.setType("Stimulation the mind");
+
+
+        EntertainmentActivity entertainmentActivity = new EntertainmentActivity();
+        entertainmentActivity.setName("Run");
+        entertainmentActivity.setTime(30);
+
+
+        System.out.println(recreationActivityReference.getName());
+        System.out.println(recreationActivityReference.getType());
+
+        Adopter adopterReference = new Adopter("Ramona");
+        adopterReference.setName("Ramona");
+        adopterReference.setGender("female");
+        adopterReference.setMoneyBugget(10000);
+
+        System.out.println(adopterReference.getName());
+        System.out.println(adopterReference.getGender());
+        System.out.println(adopterReference.getAge());
+        System.out.println(adopterReference.getMoneyBugget());
+
+
+        LocalDate expirationDate = LocalDate.of(2050, 6, 6);
+        Food foodReference = new Food("Pedigree", 29, 35, expirationDate);
+        //foodReference.setName("Pedigree");
+
+
+        foodReference.setDisponibility("True");
+       foodReference.setPrice(29);
+        foodReference.setGramsQuantity(35);
+        foodReference.expirationDate = LocalDate.of(2050, 6, 6);
+
+        System.out.println(foodReference.getName());
+        System.out.println(foodReference.isDisponibility());
+        System.out.println(foodReference.getPrice());
+        System.out.println(foodReference.isDisponibility());
+        System.out.println(foodReference.getExpirationDate());
+
+
+        adopterReference.feed(petReference, foodReference);
+
+        adopterReference.recreation(petReference, activityReference);
+
+
+        System.out.println(petReference.getSpiritLevel());
+
+
+            foodReference.expirationDate = LocalDate.now().plusYears(1).plusMonths(3).minusDays(7);
+
+
+        Toys toys = new Toys("red", "squezze");
+       toys.setColor("red");
+       toys.setSound("squezze");
+
+        System.out.println(toys.getName());
+
+        System.out.println(toys.getColor());
+        System.out.println(toys.getSound());
+
+        Snack snack = new Snack("soft", expirationDate);
+        snack.setTexture("soft");
+        System.out.println(snack.getTexture());
+
+      Pet dog = new Dog("David", 5,5,5);
+         ((Dog) dog).tag = "necklace";
+
+        System.out.println(dog.getName());
+        System.out.println(dog.getHealtyLevel());
+        System.out.println(dog.getHungryLevel());
+        System.out.println(dog.getSpiritLevel());
+        System.out.println(((Dog) dog).tag);
+
+
+        System.out.println(dog.toString());
+
+//object type determines method implementation
+dog.jumping();
+
+        Pet cat = new Cat("Kitty", 8, 3, 7);
+        ((Cat) cat).hairAccesorise = "bow clips";
+
+        System.out.println(cat.getName());
+        System.out.println(cat.getHealtyLevel());
+        System.out.println(cat.getHungryLevel());
+        System.out.println(cat.getSpiritLevel());
+
+        System.out.println(((Cat) cat).hairAccesorise);
+
+        cat.jumping();
+
 
 
 
